3a0c0126549ec3f9426ab1924933bec8db900d5c
SKHarish2507 <109017957+SKHarish2507@users.noreply.github.com>
2022-07-10 13:10:51 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Mobile {
+	public void makeCall() 
+	{
+	}
+	public void sendMsg()
+	{
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("This is my mobile");
+	}
+
+}
